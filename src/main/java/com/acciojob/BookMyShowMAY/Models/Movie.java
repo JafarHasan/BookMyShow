d4dbe0cd41328bed6414ad419699f3ddb08a2363
jafar <se.hasanjafar@gmail.com>
2024-05-14 12:55:10 +0530

    Movie class  Added

--- src/main/java/com/acciojob/BookMyShowMAY/Models/Movie.java
+++ src/main/java/com/acciojob/BookMyShowMAY/Models/Movie.java
@@ -1,5 +1,33 @@
 package com.acciojob.BookMyShowMAY.Models;
 
+import com.acciojob.BookMyShowMAY.Enum.Language;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Movie")
 public class Movie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer movieId;
+
+    @Column(unique = true)
+    private String movieName;
+
+    private Double duration;
+
+    private LocalDate releaseDate;
+
+    @Enumerated(value = EnumType.STRING)
+    private Language language;
+
+    private Double rating;
 }
